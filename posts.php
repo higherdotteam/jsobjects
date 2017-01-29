9296cbed31a1fa304de6a9d3fd1f3b16e3fca3fb
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-29 13:07:07 -0800

    changed content-type

--- posts.php
+++ posts.php
@@ -1,5 +1,21 @@
 <?php
-$data = #thedata;
+
+$arr = array(
+    'foo' => 'foo',
+    'bar' => array(
+        'baz' => 'baz',
+        'candy' => 'candy',
+        'vegetable' => array(
+            'carrot' => 'carrot',
+        )
+    ),
+    'vegetable' => array(
+        'carrot' => 'carrot2',
+    ),
+    'fruits' => 'fruits',
+);
+
+$data = $arr;
 header('Content-Type: application/json');
 echo json_encode($data);
 ?>
