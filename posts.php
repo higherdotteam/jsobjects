593514ea0276e09224074517df7940f989841e1a
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-29 21:40:25 -0800

    made data similar to fb users

--- posts.php
+++ posts.php
@@ -1,19 +1,22 @@
 <?php
 
 $arr = array(
-    'foo' => 'foo',
-    'bar' => array(
-        'baz' => 'baz',
-        'candy' => 'candy',
-        'vegetable' => array(
-            'carrot' => 'carrot',
-        )
-    ),
-    'vegetable' => array(
-        'carrot' => 'carrot2',
-    ),
-    'fruits' => 'fruits',
-);
+			array(
+				'image' => 'https://s3.amazonaws.com/uifaces/faces/twitter/chadengle/128.jpg',
+				'name' => 'Jordan Smith',
+				'text' => 'In other news, China is about to beat us to Exascale computing, believed to be the order of processing power of the human brain at neural level.',
+			),
+			array(
+				'image' => 'https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg',
+				'name' => 'Jessica Smith',
+				'text' => 'Happy New Year Everyone! We just posted on our blog about our experience at CES.',
+			),
+			array(
+				'image' => 'https://s3.amazonaws.com/uifaces/faces/twitter/zack415/128.jpg',
+				'name' => 'Garrett Smith',
+				'text' => 'I made a streaming music station for Asia but then it started playing Europe. Continental drift on an unforgivable scale. No.',
+			)
+		);
 
 $data = $arr;
 header('Content-Type: application/json');
