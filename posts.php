e4dae60476a306bcae7595236839cf2e9f62bcfd
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-30 20:09:36 -0800

    created table in db and created if statement

--- posts.php
+++ posts.php
@@ -1,9 +1,13 @@
 <?php
 $connection = @mysqli_connect('127.0.0.1', 'root', 'root') or die("Could not connect.");
 //database will be named 'fb' and table will be named 'newsfeed'
-$select_db = @mysqli_select_db('fb') or die("Could not find database.");
+$select_db = @mysqli_select_db($connection, 'fb') or die("Could not find database.");
 $query = "SELECT * FROM newsfeed";
-$execution = @mysqli_query($connection, $query) or die();
+$execution = @mysqli_query($connection, $query);
+
+if(!$execution){
+	echo "Could not perform query.";
+}
 		
 $data = $arr;
 header('Content-Type: application/json');
