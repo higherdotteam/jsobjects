fa57c76bd142ae3d9c865a940f90cb99fa822810
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-30 19:43:46 -0800

    made db connection and query

--- posts.php
+++ posts.php
@@ -1,23 +1,10 @@
 <?php
-
-$arr = array(
-			array(
-				"image" => "https://s3.amazonaws.com/uifaces/faces/twitter/chadengle/128.jpg",
-				"name" => "Jordan Smith",
-				"text" => "In other news, China is about to beat us to Exascale computing, believed to be the order of processing power of the human brain at neural level.",
-			),
-			array(
-				"image" => "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg",
-				"name" => "Jessica Smith",
-				"text" => "Happy New Year Everyone! We just posted on our blog about our experience at CES.",
-			),
-			array(
-				"image" => "https://s3.amazonaws.com/uifaces/faces/twitter/zack415/128.jpg",
-				"name" => "Garrett Smith",
-				"text" => "I made a streaming music station for Asia but then it started playing Europe. Continental drift on an unforgivable scale. No.",
-			)
-		);
-
+$connection = @mysqli_connect('127.0.0.1', 'root', 'root') or die("Could not connect.");
+//database will be named 'fb' and table will be named 'newsfeed'
+$select_db = @mysqli_select_db('fb') or die("Could not find database.");
+$query = "SELECT * FROM newsfeed";
+$execution = @mysqli_query($connection, $query) or die();
+		
 $data = $arr;
 header('Content-Type: application/json');
 echo json_encode($data);
