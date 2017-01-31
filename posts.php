bdb1b505ea48d4a337b4931c67856d1349253a6f
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-30 16:42:19 -0800

    made small changes

--- posts.php
+++ posts.php
@@ -2,19 +2,19 @@
 
 $arr = array(
 			array(
-				'image' => 'https://s3.amazonaws.com/uifaces/faces/twitter/chadengle/128.jpg',
-				'name' => 'Jordan Smith',
-				'text' => 'In other news, China is about to beat us to Exascale computing, believed to be the order of processing power of the human brain at neural level.',
+				"image" => "https://s3.amazonaws.com/uifaces/faces/twitter/chadengle/128.jpg",
+				"name" => "Jordan Smith",
+				"text" => "In other news, China is about to beat us to Exascale computing, believed to be the order of processing power of the human brain at neural level.",
 			),
 			array(
-				'image' => 'https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg',
-				'name' => 'Jessica Smith',
-				'text' => 'Happy New Year Everyone! We just posted on our blog about our experience at CES.',
+				"image" => "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg",
+				"name" => "Jessica Smith",
+				"text" => "Happy New Year Everyone! We just posted on our blog about our experience at CES.",
 			),
 			array(
-				'image' => 'https://s3.amazonaws.com/uifaces/faces/twitter/zack415/128.jpg',
-				'name' => 'Garrett Smith',
-				'text' => 'I made a streaming music station for Asia but then it started playing Europe. Continental drift on an unforgivable scale. No.',
+				"image" => "https://s3.amazonaws.com/uifaces/faces/twitter/zack415/128.jpg",
+				"name" => "Garrett Smith",
+				"text" => "I made a streaming music station for Asia but then it started playing Europe. Continental drift on an unforgivable scale. No.",
 			)
 		);
 
