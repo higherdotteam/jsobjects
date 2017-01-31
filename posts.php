88be62db7f2843b494656f2646e7469200ffae31
Christian Momdjian <chrismomdjian@gmail.com>
2017-01-30 20:53:34 -0800

    converted sql data to json

--- posts.php
+++ posts.php
@@ -1,15 +1,20 @@
 <?php
-$connection = @mysqli_connect('127.0.0.1', 'root', 'root') or die("Could not connect.");
+$connection = @mysqli_connect('127.0.0.1', 'root', 'root', 'fb') or die("Could not connect.");
 //database will be named 'fb' and table will be named 'newsfeed'
-$select_db = @mysqli_select_db($connection, 'fb') or die("Could not find database.");
 $query = "SELECT * FROM newsfeed";
 $execution = @mysqli_query($connection, $query);
 
 if(!$execution){
 	echo "Could not perform query.";
 }
-		
-$data = $arr;
+
+//converts sql data to json
+$conversion = array();
+while($row = mysqli_fetch_assoc($execution)){
+	$conversion[] = $row;
+}
+
+$data = $conversion;
 header('Content-Type: application/json');
 echo json_encode($data);
 ?>
